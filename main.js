1bcd280cde78ead60755191ebbd3e90ea2271677
duanzin <lucaduan@gmail.com>
2025-07-16 18:02:15 -0300

    funcao atualizarLista

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+import { getLista } from "./lista.js";
+
 const olItens = document.querySelector("#itens");
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
+
+atualizarLista();
+
+function atualizarLista() {
+    let lista = getLista();
+    olItens.innerHTML = "";
+    for(let i = 0; i < lista.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = lista[i];
+        olItens.appendChild(li);
+    }
+}
