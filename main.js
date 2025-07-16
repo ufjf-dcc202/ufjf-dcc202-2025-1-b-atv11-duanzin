fde1b0a274615cd8f4715c52d4727c5ddf2be370
duanzin <lucaduan@gmail.com>
2025-07-16 17:57:43 -0300

    query selectors em main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const olItens = document.querySelector("#itens");
+const pEntrada = document.querySelector("#entrada");
+const btnAdicionar = document.querySelector("#adicionar");
+const btnLimpar = document.querySelector("#limpar");
